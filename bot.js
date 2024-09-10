534ea08df14ee1a25667b873b3b6f06e84b851b4
ElizaPawky <epawky@gmail.com>
2024-09-10 18:38:45 -0400

    Update messages sent to users

--- bot.js
+++ bot.js
@@ -27,6 +27,7 @@ const AUTOMOD_CHANNEL_ID = '1283184297980723291';
 const INTRODUCTIONS_CHANNEL_ID = '1277586348626149416';
 const ASSOCIATE_ROLE_ID = '1277679159065313443'; 
 const ELIGIBLE_TO_PLAY_ROLE_ID = '1278440811876581386';
+const GENERAL_CHANNEL_ID = '1275098902743220290';
 
 // Bot ready
 client.once('ready', () => {
@@ -77,10 +78,19 @@ client.on('messageReactionAdd', async (reaction, user) => {
                             SEND_MESSAGES: true,  // Allow the user to send messages
                         });
 
-                        // Send welcome message
-                        await thread.send(`Welcome to the server, ${chosenName}! You now have access to the introductions channel. Please introduce yourself in #introductions.`);
-                        await introductionsChannel.send(`Welcome, ${chosenName}! Please introduce yourself.`);
-
+                        await thread.send(
+                            `Welcome to the server, ${chosenName}!
+                            
+                            Please introduce yourself in the #introductions channel by sharing:
+                            - Your chosen name
+                            - Your major
+                            - Your class status (freshman, junior, senior)
+                            - Which campus you're on
+                            - Your favorite dinosaur
+                            
+                            Don't forget to check out the reaction roles in the Important section. 
+                            You can react to specific messages to receive roles that will grant you access to more areas of the Discord server.`
+                        );
                         // Delete the thread
                         await thread.delete();
                     });
@@ -104,7 +114,6 @@ client.on('messageCreate', async (message) => {
     if (message.channel.id === INTRODUCTIONS_CHANNEL_ID && !message.author.bot) {
         const member = message.guild.members.cache.get(message.author.id);
 
-        // Check if the message is more than 30 characters long
         if (message.content.length > 30) {
             // Assign the Associate role
             await addRole(member, ASSOCIATE_ROLE_ID);
@@ -112,8 +121,28 @@ client.on('messageCreate', async (message) => {
             // Assign the Eligible to Play role after Associate
             await addRole(member, ELIGIBLE_TO_PLAY_ROLE_ID);
 
-            // Send confirmation message
-            await message.channel.send(`Great introduction, ${member.user.tag}! You've been given the Associate and Eligible to Play roles.`);
+            
+
+            // Post a welcome message in the general channel
+            const generalChannel = message.guild.channels.cache.get(GENERAL_CHANNEL_ID);  // Use the constant
+            if (generalChannel) {
+                await generalChannel.send(`Welcome ${member.user.tag}! We're glad you came to visit.`);
+            }
+
+        } else {
+            // Message is less than 30 characters, take the alternate action
+            
+            // Copy the message to the automod channel
+            const automodChannel = message.guild.channels.cache.get(AUTOMOD_CHANNEL_ID);
+            if (automodChannel) {
+                await automodChannel.send(`Message from ${member.user.tag} in #introductions was too short: "${message.content}"`);
+            }
+
+            // Delete (hide) the message in the introductions channel
+            await message.delete();
+
+            // DM the user
+            await member.send('Hi! Your introduction message was a bit too short. A capo will manually approve your membership within 24 hours.');
         }
     }
 });
